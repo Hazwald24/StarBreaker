311549fb8733a6fd37bd86aeec877b6c480b5fcf
elvisbosek <61314210+elvisbosek@users.noreply.github.com>
2021-08-16 11:54:37 +1000

    Ui working
    ui and menus working

--- New Unity Project/Assets/Scripts/MainMenu.cs
+++ New Unity Project/Assets/Scripts/MainMenu.cs
@@ -12,7 +12,7 @@ public class MainMenu : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        SceneManager.LoadScene(firstLevel);
+        
     }
 
     // Update is called once per frame
@@ -23,7 +23,7 @@ public class MainMenu : MonoBehaviour
 
     public void StartGame()
     {
-
+        SceneManager.LoadScene("MainScene");
     }
 
     public void OpenOptions()
